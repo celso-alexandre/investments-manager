19b89d56694729a725f516fb437b0456484b4be1
celso-alexandre <celsoalexandre@live.com>
2023-01-14 02:01:52 -0300

    [Feature] - Backend initial version (#1)
    * Finished Backend MVP 1st Phase needs (user/investmentGoal)

---
+++ backend/src/user/user.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../common/prisma.service';
+import {
+  CreateManyUserArgs,
+  CreateOneUserArgs,
+  DeleteManyUserArgs,
+  DeleteOneUserArgs,
+  FindManyUserArgs,
+  FindUniqueUserArgs,
+  UpdateOneUserArgs,
+} from './dto';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  findUnique(args: FindUniqueUserArgs) {
+    return this.prisma.user.findUniqueOrThrow(args);
+  }
+
+  findMany(args: FindManyUserArgs) {
+    return this.prisma.user.findMany(args);
+  }
+
+  createOne(args: CreateOneUserArgs) {
+    return this.prisma.user.create(args);
+  }
+
+  async createMany(args: CreateManyUserArgs) {
+    await this.prisma.user.createMany(args);
+    return true;
+  }
+
+  updateOne(args: UpdateOneUserArgs) {
+    return this.prisma.user.update(args);
+  }
+
+  deleteOne(args: DeleteOneUserArgs) {
+    return this.prisma.user.delete(args);
+  }
+
+  async deleteMany(args: DeleteManyUserArgs) {
+    await this.prisma.user.deleteMany(args);
+    return true;
+  }
+}
